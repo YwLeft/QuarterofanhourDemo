c6eb1c9f336abf54104f56defb9b400dd3106094
YwLeft <30617339+YwLeft@users.noreply.github.com>
2018-02-01 17:35:35 +0800

    Update RecommendViewPagerAdapter.java

--- app/src/main/java/com/example/asus/quarterofanhourdemo/view/adapter/RecommendViewPagerAdapter.java
+++ app/src/main/java/com/example/asus/quarterofanhourdemo/view/adapter/RecommendViewPagerAdapter.java
@@ -6,8 +6,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 import java.util.List;
 /**
- * 创建时间  2017/11/23 20:00
- * 创建人    gaozhijie
  * 类描述	ViewPager+Fragment 的适配器(推荐)
  */
 public class RecommendViewPagerAdapter extends FragmentPagerAdapter {
